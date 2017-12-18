a9b68a31b674f066aa15a48e87729138c6a9f494
Ozinuka <33878734+Ozinuka@users.noreply.github.com>
2017-12-18 20:34:40 +0100

    up
    up

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="Jeu Shinobi">
-        <meta name="author" content="chakeu_m - tran_a - Ki_jean - billed_s - luciat_j">
+        <meta name="author" content="chakeu_m">
         <link rel="icon" href="bootstrap/dist/css/favicon.ico">
 
         <title>Shinobi</title>
